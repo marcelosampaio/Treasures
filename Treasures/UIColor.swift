dd807b57b2cd9eaa6cb5076ca592c66ea368bb23
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-03-15 18:50:39 -0300

    UIColor extension

---
+++ Treasures/UIColor.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor.swift
+//  FlyToFoursquare
+//
+//  Created by Marcelo Sampaio on 12/15/16.
+//  Copyright © 2016 Marcelo Sampaio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor
+{
+    public class var applicationLightBackgroundColor: UIColor
+    {
+        return UIColor.lightGray
+    }
+    
+    public class var applicationDarkBackgroundColor: UIColor
+    {
+        return UIColor.darkGray
+    }
+    
+    public class var applicationDetailBackgroundColor: UIColor
+    {
+        return UIColor.white
+    }
+    
+    public class var applicationDetailBorderBackgroundColor: UIColor
+    {
+        return UIColor.black
+    }
+    
+}
